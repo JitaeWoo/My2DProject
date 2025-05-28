968a35caa9c48869716a99987b04c48e52fba7a3
JitaeWoo <94220257+JitaeWoo@users.noreply.github.com>
2025-05-28 15:28:31 +0900

    [Feat] PlayerManager에 PlayerStats 추가
    PlayerManager에서 PlayerStats를 관리하기 위해 ScriptableObject를 받고 이를 복사하여 PlayerStats 생성

--- Assets/Scripts/Manager/PlayerManager.cs
+++ Assets/Scripts/Manager/PlayerManager.cs
@@ -4,5 +4,12 @@ using UnityEngine;
 
 public class PlayerManager : Singleton<PlayerManager>
 {
-    
+    [SerializeField] private PlayerStats _statsTemplate;
+
+    [HideInInspector] public PlayerStats Stats;
+
+    private void Awake()
+    {
+        Stats = Instantiate(_statsTemplate);
+    }
 }
