05a1aad8d13a10e8838cb96299f9b18160bffea0
JitaeWoo <94220257+JitaeWoo@users.noreply.github.com>
2025-05-28 15:02:42 +0900

    [Feat] PlayerController 스크립트 추가
    Player의 조작과 관련된 기능을 담당할 PlayerController 스크립트 추가
    
    플레이어의 상하좌우 입력을 받아 이동을 하는 조작 구현

---
+++ Assets/Scripts/Player/PlayerController.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    private PlayerInput _input;
+    private PlayerMovement _movement;
+
+    private bool _isControl = true;
+
+    private void Awake()
+    {
+        Init();
+    }
+
+    private void Init()
+    {
+        _input = new PlayerInput();
+        _movement = GetComponent<PlayerMovement>();
+    }
+
+    void Update()
+    {
+        if (!_isControl) return;
+
+        _movement.Move(_input.MoveInput());
+    }
+}
