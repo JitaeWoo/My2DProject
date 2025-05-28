51331f9f8722913b8c18e999dc84adbd711b4969
JitaeWoo <94220257+JitaeWoo@users.noreply.github.com>
2025-05-28 15:18:52 +0900

    [Feat] PlayerStats 스크립트 생성
    플레이어의 현재 능력치를 관리하기 위한 PlayerStats 스크립트 생성
    
    ScriptableObject로 만들어서 좀 더 관리하기 편하도록 의도

---
+++ Assets/Scripts/Player/PlayerStats.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+[CreateAssetMenu(fileName = "PlayerStats", menuName = "Scriptable Objects/PlayerStats", order = 1)]
+public class PlayerStats : ScriptableObject
+{
+    public float MoveSpeed;
+}
