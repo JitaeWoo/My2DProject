9e5496ef39c44f5fec46140371dbeac7f3678002
JitaeWoo <94220257+JitaeWoo@users.noreply.github.com>
2025-05-28 17:39:48 +0900

    [Feat] PlayerState_Idle 스크립트 추가
    플레이어가 기본 상태일 때를 표현할 Idle 스크립트 추가

---
+++ Assets/Scripts/Player/PlayerStates/PlayerState_Idle.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerState_Idle : PlayerState
+{
+    public PlayerState_Idle(StateMachine stateMachine) : base(stateMachine)
+    {
+    }
+
+    public override void Update()
+    {
+
+    }
+}
