f7eeec439c799e8bf95ec8ece696d9c305d74697
JitaeWoo <94220257+JitaeWoo@users.noreply.github.com>
2025-05-28 17:34:26 +0900

    [Feat] PlayerState 스크립트 추가
    Player의 상태를 표현할 State들이 상속할 PlayerState 스크립트 추가
    
    이후에 State에 Type을 추가하여 관리할까 하는 생각에 부모로 쓸 PlayerState부터 만들었다.

---
+++ Assets/Scripts/Player/PlayerState.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerState : BaseState
+{
+    public PlayerState(StateMachine stateMachine) : base(stateMachine)
+    {
+    }
+
+    public override void Enter()
+    {
+    }
+
+    public override void Exit()
+    {
+    }
+
+    public override void Update()
+    {
+    }
+}
