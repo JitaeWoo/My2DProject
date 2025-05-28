25375fae33875930d8a5c79ee927fffb5fd20d59
JitaeWoo <94220257+JitaeWoo@users.noreply.github.com>
2025-05-28 17:50:53 +0900

    [Feat] 점프 시 착지 후 다시 점프가 가능하게 변경
    점프시 상태를 점프로 변경하여 다시 땅에 닿은 다음 점프가 가능하도록 변경.
    
    개발한 StateMachine을 적용하여 구현하였습니다.

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -7,6 +7,7 @@ public class PlayerController : MonoBehaviour
 {
     private PlayerInput _input;
     private PlayerMovement _movement;
+    private StateMachine _stateMachine;
 
     private bool _isControl = true;
 
@@ -20,17 +21,27 @@ public class PlayerController : MonoBehaviour
         Manager.Player.SetTransform(transform);
         _input = new PlayerInput();
         _movement = GetComponent<PlayerMovement>();
+        _stateMachine = new StateMachine();
+        _stateMachine.ChangeState(new PlayerState_Idle(_stateMachine));
     }
 
     void Update()
     {
         if (!_isControl) return;
 
+        _stateMachine.Update();
+
         _movement.Move(_input.MoveInput());
 
-        if (_input.JumpInput())
+        if (_input.JumpInput() && !Manager.Player.Stats.IsJump.Value)
         {
             _movement.Jump();
+            _stateMachine.ChangeState(new PlayerState_Jump(_stateMachine));
         }
+    }
+
+    private void FixedUpdate()
+    {
+        _stateMachine.FixedUpdate();
     }
 }
