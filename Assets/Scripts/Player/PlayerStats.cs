9d824d0a784884f90b534d2f406f071245cae8b2
JitaeWoo <94220257+JitaeWoo@users.noreply.github.com>
2025-05-28 15:34:38 +0900

    [Chore] PlayerStats 의미없는 공백 제거
    코드 상 의미없는 공백을 제거하여 좀 더 깔끔하게 보이도록 함

--- Assets/Scripts/Player/PlayerStats.cs
+++ Assets/Scripts/Player/PlayerStats.cs
@@ -2,7 +2,6 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-
 [CreateAssetMenu(fileName = "PlayerStats", menuName = "Scriptable Objects/PlayerStats", order = 1)]
 public class PlayerStats : ScriptableObject
 {
