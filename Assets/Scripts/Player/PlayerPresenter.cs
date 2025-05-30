ef9efbe4481d2d7f08c695d10613cc29d8f00dce
JitaeWoo <94220257+JitaeWoo@users.noreply.github.com>
2025-05-30 15:28:16 +0900

    [Feat] PlayerPresenter 스크립트 추가
    PlayerStats와 각종 외향에 영향을 끼칠 변수들을 연결해줄 PlayerPresenter 스크립트 제작

---
+++ Assets/Scripts/Player/PlayerPresenter.cs
@@ -0,0 +1,52 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerPresenter : MonoBehaviour
+{
+    private PlayerStats _stats => Manager.Player.Stats;
+
+    private SpriteRenderer _sprite;
+    private Animator _animator;
+    private void Awake()
+    {
+        _sprite = GetComponentInChildren<SpriteRenderer>();
+        _animator = GetComponent<Animator>();
+    }
+
+    private void OnEnable()
+    {
+        _stats.IsWalk.OnChanged += ChangeIsWalk;
+        _stats.IsJump.OnChanged += ChangeIsJump;
+        _stats.MoveInput.OnChanged += ChangeMoveInput;
+    }
+
+    private void OnDisable()
+    {
+        _stats.IsWalk.OnChanged -= ChangeIsWalk;
+        _stats.IsJump.OnChanged -= ChangeIsJump;
+        _stats.MoveInput.OnChanged -= ChangeMoveInput;
+    }
+
+    private void ChangeIsWalk(bool value)
+    {
+        _animator.SetBool("IsWalk", value);
+    }
+
+    private void ChangeIsJump(bool value)
+    {
+        _animator.SetBool("IsJump", value);
+    }
+
+    private void ChangeMoveInput(Vector2 value)
+    {
+        if(value.x < 0)
+        {
+            _sprite.flipX = true;
+        }
+        else
+        {
+            _sprite.flipX = false;
+        }
+    }
+}
