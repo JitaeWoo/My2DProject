32469c2e7efb619b982106d275aa11b94660b58c
JitaeWoo <94220257+JitaeWoo@users.noreply.github.com>
2025-06-04 00:03:05 +0900

    [Feat] Directions의 ToVector 확장 함수 추가
    enum인 Directions를 편하게 Vector로 바꾸기 위해 확장 함수 작성.
    
    LEFT 오타 수정.

--- Assets/Scripts/Util/Enums.cs
+++ Assets/Scripts/Util/Enums.cs
@@ -4,5 +4,20 @@ using UnityEngine;
 
 public enum Directions
 {
-    UP, DOWN, LAFT, RIGHT
+    UP, DOWN, LEFT, RIGHT
+}
+
+public static class DirectionsExtensions
+{
+    public static Vector2 ToVector(this Directions dir)
+    {
+        return dir switch
+        {
+            Directions.UP => Vector2.up,
+            Directions.DOWN => Vector2.down,
+            Directions.LEFT => Vector2.left,
+            Directions.RIGHT => Vector2.right,
+            _ => Vector2.zero
+        };
+    }
 }
