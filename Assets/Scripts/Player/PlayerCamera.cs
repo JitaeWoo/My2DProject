0af862cbbea55ffb598ff792e09324b6d86a5b03
JitaeWoo <94220257+JitaeWoo@users.noreply.github.com>
2025-06-04 15:45:50 +0900

    [Fix] 해상도에 따라 카메라 크기가 달라지는 현상 수정
    해상도와 상관없이 고정된 크기를 갖도록 코드 구현

--- Assets/Scripts/Player/PlayerCamera.cs
+++ Assets/Scripts/Player/PlayerCamera.cs
@@ -5,20 +5,27 @@ using UnityEngine;
 
 public class PlayerCamera : MonoBehaviour
 {
+    private CinemachineVirtualCamera _cam;
     private CinemachineConfiner2D _confiner;
     private bool _isInvalidate;
 
     private void Awake()
     {
         _confiner = GetComponent<CinemachineConfiner2D>();
+        _cam = GetComponent<CinemachineVirtualCamera>();
     }
 
     private void LateUpdate()
     {
         if (!_isInvalidate)
         {
+            float screenRatio = (float)Screen.height / Screen.width;
+            _cam.m_Lens.OrthographicSize = 26f * screenRatio * 0.5f;
+
             _confiner.InvalidateCache();
             _isInvalidate = true;
         }
+
+        
     }
 }
